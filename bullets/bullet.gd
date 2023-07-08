319cab735a33666f6648f5ac126eb18e838e6883
ttzck <jnttzck@gmail.com>
2023-07-08 16:06:48 +0200

    bullet wall reflection

--- bullets/bullet.gd
+++ bullets/bullet.gd
@@ -3,4 +3,10 @@ extends CharacterBody2D
 @export var speed = 100
 
 func _physics_process(delta):
-	move_and_collide(transform.basis_xform(Vector2.RIGHT) * speed * delta)
+	var bound = get_viewport_rect().size
+	if global_position.x > bound.x or global_position.x < 0:
+		rotation = PI - rotation
+	if global_position.y > bound.y or global_position.y < 0:
+		rotation *= -1
+	var fwd = transform.basis_xform(Vector2.RIGHT)
+	move_and_collide(fwd * speed * delta)
